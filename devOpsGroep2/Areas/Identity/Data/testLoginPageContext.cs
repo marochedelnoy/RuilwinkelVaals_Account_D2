3b7941951e598db234232146e7e8ea04bba1e581
marochedelnoy <marochedelnoy@icloud.com>
2021-06-16 16:36:22 +0200

    Add project files.

---
+++ devOpsGroep2/Areas/Identity/Data/testLoginPageContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using devOpsGroep2.Areas.Identity.Data;
+
+namespace devOpsGroep2.Data
+{
+    public class devOpsGroep2Context : IdentityDbContext<devOpsGroep2User>
+    {
+        public devOpsGroep2Context(DbContextOptions<devOpsGroep2Context> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ devOpsGroep2/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using devOpsGroep2.Areas.Identity.Data;
+using devOpsGroep2.Data;
+
+[assembly: HostingStartup(typeof(devOpsGroep2.Areas.Identity.IdentityHostingStartup))]
+namespace devOpsGroep2.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<devOpsGroep2Context>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("devOpsGroep2ContextConnection")));
+
+                services.AddDefaultIdentity<devOpsGroep2User>(options => options.SignIn.RequireConfirmedAccount = true)
+                    .AddEntityFrameworkStores<devOpsGroep2Context>();
+            });
+        }
+    }
+}
